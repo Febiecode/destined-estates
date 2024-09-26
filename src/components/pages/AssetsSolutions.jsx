487926def539c3ba7ec8c47417d4bfa9f808b4cd
febiecode <sathyakumar.webdev@gmail.com>
2024-09-27 02:09:29 +0530

    AssetsSolutions

--- src/components/pages/AssetsSolutions.jsx
+++ src/components/pages/AssetsSolutions.jsx
@@ -1,81 +1,96 @@
-// pages/real-estate-presence.js
-import { Player } from '@lottiefiles/react-lottie-player';
-import home1 from '../../../public/home1.json';
-import buyingSelling2 from '../../../public/buyingSelling3.json';
-import sales from '../../../public/sales.json';
+import { motion } from 'framer-motion'
 
 export default function AssetsSolutions() {
 
     return (
-        <div className="min-h-screen bg-gray-100 p-6">
+        <div className="min-h-screen bg-gray-100 p-6 flex flex-col items-center">
 
-            <div className='mb-10 bg-white p-6 rounded-lg shadow-lg flex flex-col lg:flex-row items-center'>
-                <div className='lg:w-2/5 md:me-5 my-14'>
-                    {/* Lottie Animation */}
-                    <Player
-                        autoplay
-                        loop
-                        src={home1}
-                        className="w-full max-w-md mx-auto mt-8"
+            <div className='max-w-6xl mb-10  flex flex-col lg:flex-row items-center'>
+                <div className='lg:w-1/2 md:me-5 my-5 md:my-14'>
+                    <img
+                        src="/static/Assets/client.jpg"
+                        alt="client"
+                        className="shadow-md rounded-tr-[20%] rounded-bl-[20%]"
+                        style={{ objectFit: "cover", width: "100%", height: "100%" }}
                     />
                 </div>
 
-                <div className='lg:w-3/5'>
-                    <h2 className="text-purple-950 text-3xl lg:text-4xl font-bold mb-4 text-left">Let us get you the Destined service that you desire.</h2>
-                    <p className="text-lg md:text-lg lg:text-xl !leading-relaxed">
+                <motion.div
+                    className='lg:w-1/2 lg:ms-10'
+                    initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1, ease: 'easeOut' }}
+                >
+                    <h2 className="text-purple-950 text-3xl lg:text-5xl font-bold mb-4 text-left">Let us get you the Destined service that you desire.</h2>
+                    <div className="border-b-4 border-black w-40 my-6"></div>
+                    <p className="text-lg !leading-relaxed">
                         Our full suite of real estate services will transform your buying & selling experience. We Leverage our deep knowledge of the property sites and processes, with advanced sales strategies, and streamlined workflows to provide a predictable and comprehensive experience, every time you work with us.
                     </p>
-                </div>
+                </motion.div>
 
 
             </div>
 
-            <div className='mb-10 bg-white p-6 rounded-lg shadow-lg flex flex-col-reverse lg:flex-row items-center'>
-
-                
-                <div className='lg:w-3/5 '>
-                    <h2 className="text-purple-950 text-3xl lg:text-4xl font-bold mb-4  text-left">Buying and selling of Assests</h2>
-                    <p className="text-lg md:text-lg lg:text-xl !leading-relaxed">
-                        We know your assets and the procedures better than anyone. Our client-oriented teams provide you with best-in-class service. We provide full site analysis and inspection, collocation with application of various marketing strategies in successfully selling the asset as per market value, in the same time assuring the buyer for the best return on Invesment.
-                    </p>
+            <div className='max-w-6xl mb-10  flex flex-col lg:flex-row items-center'>
+                <div className='lg:w-1/2 md:me-5 my-5 md:my-14'>
+                    <img
+                        src="/static/Assets/home7.jpg"
+                        alt="home"
+                        className="shadow-md rounded-tr-[20%] rounded-bl-[20%]"
+                        style={{ objectFit: "cover", width: "100%", height: "100%" }}
+                    />
                 </div>
 
-                <div className='lg:w-2/5 mb-20 md:mb-10 md:me-5'>
-                    {/* Lottie Animation */}
-                    <Player
-                        autoplay
-                        loop
-                        src={buyingSelling2}
-                        className="w-full max-w-md mx-auto mb-10"
+                <motion.div
+                    className='lg:w-1/2 lg:ms-10'
+                    initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1, ease: 'easeOut' }}
+                >
+                    <h2 className="text-purple-950 text-3xl lg:text-5xl font-bold mb-4 text-left">Buying and selling of Assests.</h2>
+                    <div className="border-b-4 border-black w-40 my-6"></div>
+                    <p className="text-lg !leading-relaxed">
+                        We know your assets and the procedures better than anyone. Our client-oriented teams
+                        provide you with best-in-class service. We provide full site analysis and inspection,
+                        collocation with application of various marketing strategies in successfully selling the
+                        asset as per market value, in the same time assuring the buyer for the best return on
+                        Invesment.
+                    </p>
+                </motion.div>
+
+
+            </div>
+
+            <div className='max-w-6xl mb-10  flex flex-col lg:flex-row items-center'>
+                <div className='lg:w-1/2 md:me-5 my-5 md:my-14'>
+                    <img
+                        src="/static/Assets/sell1.jpg"
+                        alt="selling"
+                        className="shadow-md rounded-tr-[20%] rounded-bl-[20%]"
+                        style={{ objectFit: "cover", width: "100%", height: "100%" }}
                     />
                 </div>
 
 
-            </div>
-
-            <div className='mb-10 bg-white p-6 rounded-lg shadow-lg flex flex-col lg:flex-row items-center'>
-
-                <div className='lg:w-2/5 md:me-5'>
-                    {/* Lottie Animation */}
-                    <Player
-                        autoplay
-                        loop
-                        src={sales}
-                        className="w-full max-w-md mx-auto mt-8"
-                    />
-                </div>
-                <div className='lg:w-3/5'>
-                    <h2 className="text-purple-950 text-3xl lg:text-4xl font-bold mb-4  text-left">Navigating Complex challenges and increasing sales bandwidth</h2>
-                    <p className="text-lg md:text-lg lg:text-xl !leading-relaxed">
-                        It takes dedicated resources and specialized knowledge to accomplish a successful sale. To which our team works tirelessly integrating complex coordination along with navigating regulatory processes and obtaining precise market value for each and every property for a smooth transition of sale. Even with resources in place, managing sale timelines, coordinating with investors, sellers, landlords, and monitoring quality control of services offered and adherence to ensuring masterful negotiation strategies requires a high level of oversight and takes resources from our core business.
+                <motion.div
+                    className='lg:w-1/2 lg:ms-10'
+                    initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1, ease: 'easeOut' }}
+                >
+                    <h2 className="text-purple-950 text-3xl lg:text-5xl font-bold mb-4 text-left">Navigating Complex challenges and increasing sales bandwidth</h2>
+                    <div className="border-b-4 border-black w-40 my-6"></div>
+                    <p className="text-lg !leading-relaxed">
+                        It takes dedicated resources and specialized knowledge to accomplish a successful sale.
+                        To which our team works tirelessly integrating complex coordination along with navigating
+                        regulatory processes and obtaining precise market value for each and every property for a
+                        smooth transition of sale. Even with resources in place, managing sale timelines,
+                        coordinating with investors, sellers, landlords, and monitoring quality control of services
+                        offered and adherence to ensuring masterful negotiation strategies requires a high level of
+                        oversight and takes resources from our core business.
                     </p>
-                </div>
-
-
+                </motion.div>
             </div>
-
-
-
         </div>
     );
 }
